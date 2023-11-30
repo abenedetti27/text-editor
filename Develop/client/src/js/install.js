310085b06b7bd1911aac6f853c3f29f77bad2e89
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-11-29 22:25:50 -0800

    handler for the `appinstalled` event

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -29,4 +29,6 @@ butInstall.addEventListener('click', async () => {
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('app installed', event);
+});
