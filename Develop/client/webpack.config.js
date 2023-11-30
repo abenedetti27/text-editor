6d2541f06ed625ed2446c2347b5dbd9940cdc19d
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-11-29 22:51:42 -0800

    configure workbox plugins, CSS loaders and babel

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -14,16 +14,59 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'text-editor.bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Text Editor'
+      }),
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'Text Editor',
+        description: 'A simple text editor',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', 
+        icons: [
+          {
+            src: path.resolve('./images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512] 
+          },
+          {
+            src: path.resolve('./images/logo.png'),
+            size: '1024x1024' 
+          },
+          {
+            src: path.resolve('./favicon.ico'),
+            size: '1024x1024',
+            purpose: 'maskable'
+          }
+        ]
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js'
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        { 
+          test: /\.js$/, 
+          exclude: /node_modules/, 
+          use: ['babel-loader'],
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"],
+
+            },
+        },
       ],
     },
   };
